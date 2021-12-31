e6ec650d9c813779a9dd5ef540f19681828e758b
franco <x.francoruiz@gmail.com>
2021-12-31 19:39:37 -0300

    create get function

--- src/components/Cats.vue
+++ src/components/Cats.vue
@@ -1,13 +1,29 @@
 <template>
-    <h1>Hello cats</h1>
+  <h1>{{ title }}</h1>
+  <img :src="imgCats" />
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      title: "Random Cats!",
+      imgCats: null,
+    };
+  },
+  methods: {
+    async getRandomCats() {
+      const { status, url } = await fetch("https://cataas.com/cat/gif");
+
+      if (status !== 200) return;
+
+      this.imgCats = url;
+    },
+  },
+  beforeMount() {
+    this.getRandomCats();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
