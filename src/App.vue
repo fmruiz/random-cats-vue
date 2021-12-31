cf39bc090ddfe2003d14e851df9ae13fe200c86c
franco <x.francoruiz@gmail.com>
2021-12-31 19:11:56 -0300

    create cats component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Cats />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cats from './components/Cats.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cats
   }
 }
 </script>
---
+++ src/components/Cats.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello cats</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
